df213cefb15c384b5e4c3a3b273090cd230c1c8e
Carmine Di Martino <cardimart@gmail.com>
2018-10-28 18:28:42 +0100

    Clean up folder

--- app.js
+++ app.js
@@ -1,207 +1,319 @@
+// import { populateInfoWindow } from "./util/mapBehaviour.js";
+
 // Create a new blank array for all the listing markers.
-var markers = [];
-var map;
-var largeInfowindow;
+let markers = [];
+let map;
+let largeInfowindow;
 
 var app = new Vue({
+  // the root element that will be compiled
+  el: "#container",
+  // app initial state
+  data: {
+    options: [
+      "Raised less than $20M",
+      "Raised $20-$50M",
+      "Raised more than $50M",
+      "Any"
+    ],
+    selection: "",
+    // These are the real estate listings that will be shown to the user.
+    // Normally we'd have these in a database instead.
+    locations: [
+      {
+        title: "Bitfury",
+        location: {
+          lat: 52.374192,
+          lng: 4.886953
+        },
+        fundingRaised: 40.0,
+        domain: "bitfury.com",
+        selectedByUser: true,
+        description:
+          "Bitfury is the leading full service bitcoin and blockchain technology company.",
+        descriptionIsHidden: true,
+        jobs: []
+      },
+      {
+        title: "WeTransfer",
+        location: {
+          lat: 52.333458,
+          lng: 4.916989
+        },
+        fundingRaised: 25.0,
+        domain: "wetransfer.com",
+        selectedByUser: true,
+        description:
+          "WeTransfer offers a way to send files around the world through its platform.",
+        descriptionIsHidden: true,
+        jobs: []
+      },
+      {
+        title: "MessageBird",
+        location: {
+          lat: 52.344234,
+          lng: 4.911617
+        },
+        fundingRaised: 60.1,
+        domain: "messagebird.com",
+        selectedByUser: true,
+        description:
+          "MessageBird is a cloud communications platform that connects enterprises to their global customers",
+        descriptionIsHidden: true,
+        jobs: []
+      },
+      {
+        title: "Framer",
+        location: {
+          lat: 52.37276,
+          lng: 4.88816
+        },
+        fundingRaised: 9.3,
+        domain: "framer.com",
+        selectedByUser: true,
+        description:
+          "Framer is a tool to design interactive high-fidelity prototypes for iOS, Android, desktop, or the web.",
+        descriptionIsHidden: true,
+        jobs: []
+      },
+      {
+        title: "Adyen",
+        location: {
+          lat: 52.376501,
+          lng: 4.905894
+        },
+        fundingRaised: 266.0,
+        domain: "ayden.com",
+        selectedByUser: true,
+        description:
+          "Adyen is a multichannel payment company outsourcing payment services to international merchants.",
+        descriptionIsHidden: true,
+        jobs: []
+      },
+      {
+        title: "Etergo",
+        location: {
+          lat: 52.352816,
+          lng: 4.841205
+        },
+        fundingRaised: 6.1,
+        domain: "etergo.com",
+        selectedByUser: true,
+        description:
+          "Etergo is an automotive company that develops AppScooter, an electric smart vehicle for the future-ready industry.",
+        descriptionIsHidden: true,
+        jobs: []
+      },
+      {
+        title: "Impraise",
+        location: {
+          lat: 52.361258,
+          lng: 4.899946
+        },
+        fundingRaised: 13.6,
+        domain: "impraise.com",
+        selectedByUser: true,
+        description:
+          "Impraise is a web and mobile app for sharing actionable, timely feedback between colleagues.",
+        descriptionIsHidden: true,
+        jobs: []
+      },
+      {
+        title: "Treatwell",
+        location: {
+          lat: 52.375289,
+          lng: 4.891824
+        },
+        fundingRaised: 38.0,
+        domain: "treatwell.com",
+        selectedByUser: true,
+        description:
+          "Treatwell is Europe's leading marketplace for booking hair and beauty appointments online.",
+        descriptionIsHidden: true,
+        jobs: []
+      }
+    ]
+  },
 
-    // the root element that will be compiled
-    el: '#container',
-
-    // app initial state
-    data: {
-        city: 'NYC',
-        selected: '',
-        // These are the real estate listings that will be shown to the user.
-        // Normally we'd have these in a database instead.
-        locations: [{
-                title: 'Park Ave Penthouse',
-                location: {
-                    lat: 40.7713024,
-                    lng: -73.9632393
-                }
-            },
-            {
-                title: 'Chelsea Loft',
-                location: {
-                    lat: 40.7444883,
-                    lng: -73.9949465
-                }
-            },
-            {
-                title: 'Union Square Open Floor Plan',
-                location: {
-                    lat: 40.7347062,
-                    lng: -73.9895759
-                }
-            },
-            {
-                title: 'East Village Hip Studio',
-                location: {
-                    lat: 40.7281777,
-                    lng: -73.984377
-                }
-            },
-            {
-                title: 'TriBeCa Artsy Bachelor Pad',
-                location: {
-                    lat: 40.7195264,
-                    lng: -74.0089934
-                }
-            },
-            {
-                title: 'Chinatown Homey Space',
-                location: {
-                    lat: 40.7180628,
-                    lng: -73.9961237
-                }
-            }
-        ]
+  methods: {
+    initMap: function() {
+      // Constructor creates a new map - only center and zoom are required.
+      map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+          lat: 52.37077,
+          lng: 4.900014
+        },
+        zoom: 13,
+        mapTypeControl: false
+      });
+      this.createMarkers();
     },
 
-    methods: {
+    createMarkers: function() {
+      setMapOnAll(null);
+      markers.length = 0; // empty array of markers
+      largeInfowindow = new google.maps.InfoWindow();
+      var _this = this;
+      locations = this.locations;
 
-        // This function will loop through the markers array and display them all.
-        showListings: function() {
-            var bounds = new google.maps.LatLngBounds();
-            // Extend the boundaries of the map for each marker and display the marker
-            for (var i = 0; i < markers.length; i++) {
-                markers[i].setMap(map);
-                bounds.extend(markers[i].position);
-            }
-            map.fitBounds(bounds);
-        },
-
-
-        // This function will loop through the listings and hide them all.
-        hideListings: function() {
-            for (var i = 0; i < markers.length; i++) {
-                markers[i].setMap(null);
-            }
-        },
-
-
-        // This function takes in a COLOR, and then creates a new marker
-        // icon of that color. The icon will be 21 px wide by 34 high, have an origin
-        // of 0, 0 and be anchored at 10, 34).
-        makeMarkerIcon: function(markerColor) {
-            var markerImage = new google.maps.MarkerImage(
-                'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
-                '|40|_|%E2%80%A2',
-                new google.maps.Size(21, 34),
-                new google.maps.Point(0, 0),
-                new google.maps.Point(10, 34),
-                new google.maps.Size(21, 34));
-            return markerImage;
-        },
-
-
-        // This function populates the infowindow when the marker is clicked. We'll only allow
-        // one infowindow which will open at the marker that is clicked, and populate based
-        // on that markers position.
-        populateInfoWindow: function(marker, infowindow) {
-            // Check to make sure the infowindow is not already opened on this marker.
-            if (infowindow.marker != marker) {
-                infowindow.marker = marker;
-                infowindow.setContent('<div>' + marker.title + '</div>');
-                infowindow.open(map, marker);
-                // Make sure the marker property is cleared if the infowindow is closed.
-                infowindow.addListener('closeclick', function() {
-                    console.log(this);
-                    infowindow.marker = null;
-                });
-            }
-        },
-
-        initMap: function() {
-            // Constructor creates a new map - only center and zoom are required.
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: {
-                    lat: 52.367356,
-                    lng: 4.907849
-                },
-                zoom: 17,
-                mapTypeControl: false
-            });
-            largeInfowindow = new google.maps.InfoWindow()
-            // var largeInfowindow = new google.maps.InfoWindow();
-            var _this = this;
-            locations = this.locations;
-            // Style the markers a bit. This will be our listing marker icon.
-            // var defaultIcon = makeMarkerIcon('0091ff');
-
-            // Create a "highlighted location" marker color for when the user
-            // mouses over the marker.
-            // var highlightedIcon = makeMarkerIcon('FFFF24');
-
-            // The following group uses the location array to create an array of markers on initialize.
-            for (var i = 0; i < locations.length; i++) {
-                // Get the position from the location array.
-                var position = locations[i].location;
-                var title = locations[i].title;
-                // Create a marker per location, and put into markers array.
-                var marker = new google.maps.Marker({
-                    position: position,
-                    title: title,
-                    animation: google.maps.Animation.DROP,
-                    id: i
-                });
-                // Push the marker to our array of markers.
-                markers.push(marker);
-                // Create an onclick event to open an infowindow at each marker.
-                marker.addListener('click', _this.populateInfoWindow(marker, largeInfowindow));
-                console.log(title);
-            } 
-        },
-
-
-        initFoursquare: function() {
-            console.log("Run foursquare");
-        //    console.log(response);
-        },
-
-
-        // Return error if the Google Maps API did not load
-        mapsAPIError: function() {
-            alert("Google Maps is not working. Try again later.");
-        },
-
-
-        // Return error if the Foursquare API did not load
-        foursquareAPIError: function() {
-            alert("Foursquare is not working. Try again later.");
+      // The following group uses the location array to create an array of markers on initialize.
+      for (var i = 0; i < locations.length; i++) {
+        if (locations[i].selectedByUser) {
+          // Get the position from the location array.
+          var position = locations[i].location;
+          var title = locations[i].title;
+          // Create a marker per location, and put into markers array.
+          var marker = new google.maps.Marker({
+            position: position,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            id: i,
+            icon: "./static/simple-icon-border.png",
+            size: new google.maps.Size(27, 41),
+            scaledSize: new google.maps.Size(27, 41)
+          });
+          // Push the marker to our array of markers.
+          markers.push(marker);
+          // Create an onclick event to open an infowindow at each marker.
+          marker.addListener("click", function() {
+            populateInfoWindow(this, largeInfowindow);
+          });
+          marker.addListener("click", toggleBounce);
         }
-    } // /Methods
-});
+      }
+      this.showListings();
 
+      // This function populates the infowindow when the marker is clicked. We'll only allow
+      // one infowindow which will open at the marker that is clicked, and populate based
+      // on that markers position.
+      function populateInfoWindow(marker, infowindow) {
+        // Check to make sure the infowindow is not already opened on this marker.
+        if (infowindow.marker != marker) {
+          infowindow.marker = marker;
+          infowindow.setContent(`<div style="min-width: 120px; vertical-align: middle;">
+            <img src="https://logo.clearbit.com/${
+              marker.title
+            }.com?size=50" style="border-radius: 6px; vertical-align: middle; text-align: center; margin: 8px; hight: 50px; object-fit: cover;" class="custom-font" {
+              constructor() {
 
-var request = new XMLHttpRequest();
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
-request.onload = function() {
+              }
+          }>${marker.title}</div>`);
+          // ^^ FIX The logos. Ayden is either stretched or to tall enough
+          infowindow.open(map, marker);
+          // Make sure the marker property is cleared if the infowindow is closed.
+          infowindow.addListener("closeclick", function() {
+            console.log(this);
+            infowindow.marker = null;
+          });
+        }
+      }
 
-    // Begin accessing JSON data here
-    var data = JSON.parse(this.response);
-    if (request.status >= 200 && request.status < 400) {
-        data.forEach(movie => {
-            const card = document.createElement('div');
-            card.setAttribute('class', 'card');
+      function toggleBounce() {
+        _this = this;
+        _this.setAnimation(google.maps.Animation.BOUNCE);
+        setInterval(function() {
+          _this.setAnimation(null);
+        }, 3400);
+        // TO FIX. Use promises to fix the fact that if you click on a new location
+        // while the previous marker is still bouncing, that marker will bounce forever
+      }
 
-            const h1 = document.createElement('h1');
-            h1.textContent = movie.title;
+      // Sets the map on all markers in the array.
+      function setMapOnAll(map) {
+        for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(map);
+        }
+      }
+    },
 
-            const p = document.createElement('p');
-            movie.description = movie.description.substring(0, 300);
-            p.textContent = `${movie.description}...`;
+    makeMarkerBounce: function(location) {
+      for (const m of markers) {
+        if (m.title == location.title) google.maps.event.trigger(m, "click");
+      }
+    },
 
-            container.appendChild(card);
-            card.appendChild(h1);
-            card.appendChild(p);
-        });
-    } else {
-        const errorMessage = document.createElement('marquee');
-        errorMessage.textContent = `Gah, it's not working!`;
-        app.appendChild(errorMessage);
+    showDesc: function(location) {
+      if (location.descriptionIsHidden) {
+        location.descriptionIsHidden = false;
+      } else {
+        location.descriptionIsHidden = true;
+      }
+      getJobListing(location);
+
+      function getJobListing(location) {
+        if (!location.jobs.length) {
+          fetch(
+            "https://api.lever.co/v0/postings/" + location.title.toLowerCase()
+          )
+            .then(response => {
+              if (!response.ok) {
+                throw Error(response.statusText);
+              }
+              return response.json();
+            })
+            .then(data => {
+              console.log(typeof data);
+              console.log(data);
+              for (const companyData of data) {
+                console.log(companyData.text);
+                console.log(typeof companyData.text);
+                location.jobs.push(companyData.text);
+              }
+            })
+            .catch(err => {
+              console.log("Level API is not working. Try again later.");
+            });
+        }
+      }
+    },
+
+    listItemClick: function(location) {
+      this.makeMarkerBounce(location);
+      this.showDesc(location);
+    },
+
+    // This function will loop through the markers array and display them all.
+    showListings: function() {
+      var bounds = new google.maps.LatLngBounds();
+      // Extend the boundaries of the map for each marker and display the marker
+      for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+        bounds.extend(markers[i].position);
+      }
+      map.fitBounds(bounds);
+    },
+
+    // This function will loop through the listings and hide them all.
+    hideListings: function() {
+      for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+      }
+    },
+
+    updateSelection: function() {
+      selection = this.selection;
+      options = this.options;
+      for (const loc of this.locations) {
+        if (selection == options[0]) {
+          if (loc.fundingRaised < 20) loc.selectedByUser = true;
+          else loc.selectedByUser = false;
+        } else if (selection == options[1]) {
+          if (loc.fundingRaised > 20 && loc.fundingRaised < 50)
+            loc.selectedByUser = true;
+          else loc.selectedByUser = false;
+        } else if (selection == options[2]) {
+          if (loc.fundingRaised > 50) loc.selectedByUser = true;
+          else loc.selectedByUser = false;
+        } else {
+          loc.selectedByUser = true;
+        }
+      }
+      this.createMarkers();
+    },
+
+    // Return error if the Google Maps API did not load
+    mapsAPIError: function() {
+      alert("Google Maps is not working. Try again later.");
     }
-}
-
-request.send();
+  }
+});
